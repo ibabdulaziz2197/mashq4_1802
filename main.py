b68db72c03feaf75ed2e528b7c511e7a3378fc9b
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2026-02-18 16:48:56 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# 1
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+result = A & B
+print(result)
+
+
+# 2
+A = {10, 20, 30}
+B = {30, 40, 50}
+
+result = A | B
+print(result)
+
+
+# 3
+numbers = {5, 10, 15, 20, 25}
+print(len(numbers))
+
+# 4
+numbers = {100, 200, 300, 400}
+print(numbers.remove(300))
